772665015576ec8f6c096edc7d15e599a7ac0213
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-07-31 12:48:56 -0700

    App.js and components setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import ReactDOM from "react-dom";
 import "./App.css";
 
-function App() {
+import NasaData from './components/NasaData';
+
+export default function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=hkcG8e1aPSbF5lPiZWqd8qSGPuTOaYhbjKwO9auP')
+      .then(response => {
+        const photo = response.data;
+        console.log(response);
+        setData(photo);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        Test
       </p>
     </div>
   );
 }
 
-export default App;
---
+++ src/components/NasaData.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function NasaData() {
+
+  const [nasaState, setNasaState] = useState([]);
+
+  return (
+    <div>
+      {nasaState.map((value) => {
+        return
+      })}
+    </div>
+  )
+}
